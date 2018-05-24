30b9f99ddb73f3ed91183da22048805181649ac1
KalusB <benedict.kalus+github@port.ac.uk>
2018-05-24 11:33:52 +0200

    Add files via upload

--- template_fit.pyx
+++ template_fit.pyx
@@ -1,3 +1,20 @@
+'''
+    Programme to fit polynomials to the ratio of observed galaxies to
+    expected galaxies as a function of a contaminant, and to generate
+    templates in configuration space based on that information
+    Copyright (C) 2018  Benedict Kalus
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 import numpy as np
 cimport numpy as np
 import healpy as hp
