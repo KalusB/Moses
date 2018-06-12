6b97008df5e23047c27e5a2eb46e26b187a7287a
KalusB <benedict.kalus+github@port.ac.uk>
2018-06-12 16:29:35 +0200

    Update template_fit.pyx
    Include ifib2 dependent fitting routine

--- template_fit.pyx
+++ template_fit.pyx
@@ -20,9 +20,10 @@ cimport numpy as np
 import healpy as hp
 import fitsio
 from scipy.integrate import quad
+import time
 import GalaxyvsContaminant
 
-cdef int NSIDE=GalaxyvsContaminant.NSIDE
+cdef int NSIDE=64
 cdef int NX, NY, NZ
 NX=NY=NZ=128
 cdef double XMIN = -1800.
@@ -36,7 +37,7 @@ cdef double dx  = float(XMAX-XMIN)/NX
 cdef double dy  = float(YMAX-YMIN)/NY
 cdef double dz  = float(ZMAX-ZMIN)/NZ
 
-def loadsyst(char* systfilename):
+def loadsyst(char* systfilename,char* oi='NESTED'):
 	cdef np.ndarray fsys=np.loadtxt(systfilename)
 	cdef np.ndarray mask=np.zeros(len(fsys))
 	cdef int cell, neighbour
@@ -49,49 +50,75 @@ def loadsyst(char* systfilename):
 				if fsys[neighbour]==0:
 					mask[cell]-=0.25
 	fsys/=mask
+	cdef int NSIDEin=hp.pixelfunc.get_nside(fsys)
+	mask=hp.ud_grade(mask,nside_out=NSIDE,order_in=oi, order_out='RING')
+	fsys=hp.ud_grade(fsys,nside_out=NSIDE,order_in=oi, order_out='RING')
+	fsys*=float(hp.nside2npix(NSIDEin))/hp.nside2npix(NSIDE)
 	return fsys, mask
 
-def polynomial2invweight(np.ndarray p1, np.ndarray fsys, np.ndarray Wtot):
+def polynomial2invweight(np.ndarray p1, np.ndarray fsys, np.ndarray Wtot, char* ifib2filename=""):
 	cdef np.ndarray W=np.zeros(NX*NY*NZ)
-	cdef np.ndarray f1=np.poly1d(p1)(fsys)
-	cdef double xmin, xmax, ymin, ymax, zmin, zmax, dpmin, dpmax, dp, x, y, z, dec, ra
+	cdef bint ifib2flag=(len(ifib2filename)>0)
+	cdef np.ndarray f1=np.empty([len(p1),len(fsys)]), ifib2av
+	if ifib2flag:
+		ifib2av=np.loadtxt(ifib2filename)
+		for i in np.arange(len(p1)):
+			f1[i,:]=np.poly1d(p1[i])(fsys)
+	else:
+		f1=np.poly1d(p1)(fsys)
+	cdef double dp, x, y, z, dec, ra, ifib2
+	cdef int ifib2ind
 	for ix in np.arange(NX):
 		for iy in np.arange(NY):
 			for iz in np.arange(NZ):
 				ipos = iz+NZ*(iy+NY*ix)
-				xmin=xmax=ymin=ymax=zmin=zmax=0
-				if ix*dx+XMIN<0:
-					xmin=(ix+1)*dx+XMIN
-					xmax=ix*dx+XMIN
-				else:
-					xmin=ix*dx+XMIN
-					xmax=(ix+1)*dx+XMIN
-				if iy*dy+YMIN<0:
-					ymin=(iy+1)*dy+YMIN
-					ymax=iy*dy+YMIN
-				else:
-					ymin=iy*dy+YMIN
-					ymax=(iy+1)*dy+YMIN
-				if iz*dz+ZMIN<0:
-					zmin=(iz+1)*dz+ZMIN
-					zmax=iz*dz+ZMIN
-				else:
-					zmin=iz*dz+ZMIN
-					zmax=(iz+1)*dz+ZMIN
-				dpmin=np.sqrt(xmin*xmin+ymin*ymin+zmin*zmin)
-				dpmax=np.sqrt(xmax*xmax+ymax*ymax+zmax*zmax)
-				dp=(dpmin+dpmax)/2.
-				x=(xmin+xmax)/2.
-				y=(ymin+ymax)/2.
-				z=(zmin+zmax)/2.
+				x=(ix+0.5)*dx+XMIN
+				y=(iy+0.5)*dy+YMIN
+				z=(iz+0.5)*dz+ZMIN
+				dp=np.sqrt(x*x+y*y+z*z)
 				dec=np.arcsin(z/dp)
 				ra=np.arctan2(y,x)
 				pix=hp.pixelfunc.ang2pix(NSIDE,-dec+0.5*np.pi,ra)
-				W[ipos]=f1[pix]-Wtot[ipos]
+				if ifib2flag:
+					ifib2=ifib2av[np.searchsorted(ifib2av[1,:],dp),2]
+					ifib2ind=np.searchsorted(np.arange(20.3,21.8,0.3),ifib2)
+					W[ipos]=f1[ifib2ind,pix]-Wtot[ipos]
+				else:
+					W[ipos]=f1[pix]-Wtot[ipos]
 	Wtot+=W
 	return W
 
-def main(char* systfilename, char* label, char* rhorfilename, double minfsys, double maxfsys, char[:,:] files, char[:,:] randoms, double redshiftmin=0.43, double redshiftmax=0.7, int totdeg=3, char* GalaxyvsContaminantfile=None):
+def main_ifib2(char* systfilename, char* label, char* rhorfilename, double minfsys, double maxfsys, char[:,:] files, char[:,:] randoms, char* GalaxyvsContaminantfile, char* ifib2filename, double redshiftmin=0.43, double redshiftmax=0.7, int totdeg=3):
+	cdef np.ndarray fsys, mask
+	fsys, mask = loadsyst(systfilename)
+	cdef int deg, rhobar, ix, iy, iz, ipos, pix
+	cdef np.ndarray rho_r=np.loadtxt(rhorfilename)
+	cdef np.ndarray data1, data2, data3, data4, data5
+	data1=np.loadtxt(GalaxyvsContaminantfile[0])
+	data2=np.loadtxt(GalaxyvsContaminantfile[1])
+	data3=np.loadtxt(GalaxyvsContaminantfile[2])
+	data4=np.loadtxt(GalaxyvsContaminantfile[3])
+	data5=np.loadtxt(GalaxyvsContaminantfile[4])
+	cdef np.ndarray Wtot=np.ones(NX*NY*NZ)
+	cdef np.ndarray f=np.zeros((NX*NY*NZ,totdeg))
+	cdef np.ndarray W, fwbar, pos, p1
+	cdef double Wrhobar_over_rhobar
+	for deg in np.arange(1,totdeg+1):
+		fwbar=np.zeros(NX*NY*NZ)
+		pos=np.zeros(NX*NY*NZ)
+		p1=np.array([np.polyfit(data1[:,0],data1[:,1],deg,w=1./data1[:,2]), np.polyfit(data2[:,0],data2[:,1],deg,w=1./data2[:,2]), np.polyfit(data3[:,0],data3[:,1],deg,w=1./data3[:,2]), np.polyfit(data4[:,0],data4[:,1],deg,w=1./data4[:,2]), np.polyfit(data5[:,0],data5[:,1],deg,w=1./data5[:,2])])
+		W=polynomial2invweight(p1, Wtot, ifib2filename)
+		Wrhobar_over_rhobar=np.dot(rho_r,W)/np.sum(rho_r)
+		for ix in np.arange(NX):
+			for iy in np.arange(NY):
+				for iz in np.arange(NZ):
+					ipos = iz+NZ*(iy+NY*ix)
+					f[ipos][deg-1]=(W[ipos]-Wrhobar_over_rhobar)*rho_r[ipos]
+	cdef np.ndarray fout=np.zeros(NX*NY*NZ)
+	for deg in np.arange(1,totdeg+1):
+		np.savetxt("f"+label+str(deg)+"_NGC_BOX128.txt",f[:,deg-1])
+
+def main(char* systfilename, char* label, char* rhorfilename, double minfsys, double maxfsys, char[:,:] files, char[:,:] randoms, double redshiftmin=0.43, double redshiftmax=0.7, int totdeg=3, GalaxyvsContaminantfile=None):
 	cdef np.ndarray fsys, mask
 	fsys, mask = loadsyst(systfilename)
 	cdef int deg, rhobar, ix, iy, iz, ipos, pix
@@ -110,8 +137,9 @@ def main(char* systfilename, char* label, char* rhorfilename, double minfsys, do
 		pos=np.zeros(NX*NY*NZ)
 		p1=np.polyfit(data1[:,0],data1[:,1],deg,w=1./data1[:,2])
 		W=polynomial2invweight(p1, Wtot)
-		Wrhobar_over_rhobar=np.dot(rho_r*W)/np.sum(rho_r)
+		Wrhobar_over_rhobar=np.dot(rho_r,W)/np.sum(rho_r)
 		for ipos in np.arange(NX*NY*NZ):
 			f[ipos][deg-1]=(W[ipos]-Wrhobar_over_rhobar)*rho_r[ipos]
+	cdef np.ndarray fout=np.zeros(NX*NY*NZ)
 	for deg in np.arange(1,totdeg+1):
 		np.savetxt("f"+label+str(deg)+"_NGC_BOX128.txt",f[:,deg-1])
